96f64ca655a718e0f99cc9e69ae106e8bd6803c7
NomanNasirMinhas <sp17bsse010@gmail.com>
2022-06-18 13:04:36 +0500

    initial commit

---
+++ src/db/consts.rs
@@ -0,0 +1,2 @@
+pub const MONGO_URI: &str = "mongodb+srv://spidy_admin:CsIcBnIcP786@thetamiddleware.pi0h1.gcp.mongodb.net/?retryWrites=true&w=majority";
+pub const MONGO_DB_NAME: &str = "rust_test";
