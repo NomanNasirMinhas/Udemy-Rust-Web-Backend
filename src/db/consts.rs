1daceaa37d4affc6d31efc1ec3799d221cce225c
Noman Nasir Minhas <sp17bsse010@gmail.com>
2022-06-18 13:33:45 +0500

    Update consts.rs

--- src/db/consts.rs
+++ src/db/consts.rs
@@ -1,2 +1,2 @@
-pub const MONGO_URI: &str = "mongodb+srv://spidy_admin:CsIcBnIcP786@thetamiddleware.pi0h1.gcp.mongodb.net/?retryWrites=true&w=majority";
-pub const MONGO_DB_NAME: &str = "rust_test";
+pub const MONGO_URI: &str = YOUR_URL_HERE;
+pub const MONGO_DB_NAME: &str = YOUR_DB_NAME;
